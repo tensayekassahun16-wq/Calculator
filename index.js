fc18a84b6e4f061b1168601ccb8a0e2440520272
Tensaye <257618128+tensayekassahun16-wq@users.noreply.github.com>
2026-09-03 23:55:05 +0300

    Addition going great phase one and a half
    looks like my logic was great for identifing operator
    and number places and behaves accordingly, very good
    --my internal monologue is saying i'm a genius--

--- index.js
+++ index.js
@@ -2,18 +2,45 @@ const numbers = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operator");
 let count = 0;
 let operation = [];
+let result;
+
+function operationCalculator(arr){
+    if(arr[1] === '+'){
+        console.log(arr[0] + arr[2]);
+        count = 0;
+        arr.length = 0;
+        console.log(count);
+        console.log(arr.length);
+    }
+}
 
 numbers.forEach((number)=>{
-    number.addEventListener(()=>{
+    number.addEventListener("click", ()=>{
         if(count%2===0){
-            operation.push(number);
+            operation.push(+(number.textContent));
             count++;
+            console.log(operation);
             if(operation.length === 3){
                 //call calculating function
+                operationCalculator(operation);
+                //store value inside a variable and maybe the first slot of the array
             }
         }
         else{
             //do nothing?
+        }
+    });
+});
+
+operators.forEach((operator)=>{
+    operator.addEventListener("click", ()=>{
+        if(count%2!==0){
+            operation.push(operator.textContent);
+            count++;
+            console.log(operation);
+        }
+        else{
+            //idk what to put
         }
     });
 });
